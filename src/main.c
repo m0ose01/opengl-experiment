d55eed4f83883e99fcd2f63a5d66be05baa5acde
Seamus <m0ose01@proton.me>
2025-07-14 00:38:27 +0100

    Vary triangle colour with time, via uniform.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include <hello.h>
 
 #include <glad/glad.h>
@@ -34,11 +35,9 @@ int main(void)
 	const char *vertexShaderSource =
 		"#version 330 core\n"
 		"layout (location = 0) in vec3 aPos;\n"
-		"out vec4 vertexColor;\n"
 		"void main()\n"
 		"{\n"
 		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-		"   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
 		"}\0";
 
 	unsigned int vertexShader;
@@ -59,11 +58,11 @@ int main(void)
 	const char* fragmentShader1Source =
 		"#version 330 core\n"
 		"out vec4 FragColor;\n"
-		"in vec4 vertexColor;\n"
+		"uniform vec4 ourColor;\n"
 
 		"void main()\n"
 		"{\n"
-		"    FragColor = vertexColor;\n"
+		"    FragColor = ourColor;\n"
 		"}\n";
 
 	const char* fragmentShader2Source =
@@ -176,7 +175,12 @@ int main(void)
 		glClearColor(0.2, 0.2, 0.2, 1.0);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		float timeValue = glfwGetTime();
+		float greenValue = (sin(timeValue) / 0.5f) + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram1, "ourColor");
+
 		glUseProgram(shaderProgram1);
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 		glBindVertexArray(VAO[0]);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glUseProgram(shaderProgram2);
