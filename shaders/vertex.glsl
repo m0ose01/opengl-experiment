86b13c105399fedd9eb949fc6dc324be7157a97f
Seamus <m0ose01@proton.me>
2025-07-14 02:29:58 +0100

    Base colour on vertex position.

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -3,11 +3,11 @@
 uniform float offsetX;
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
-out vec3 ourColor;
+out vec4 ourColor;
 
 void main()
 {
-	gl_Position = vec4(aPos.x + offsetX, -aPos.y, aPos.z, 1.0);
-	ourColor = aColor;
+	gl_Position = vec4(aPos.x + offsetX, aPos.y, aPos.z, 1.0);
+	ourColor = gl_Position;
 };
 
