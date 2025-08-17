afd95b1c8ad7e3aa2892e9c144906c4ae7970ad8
Seamus <m0ose01@proton.me>
2025-08-17 12:53:17 +0100

    Revert "Only show centre of image."
    This reverts commit 8246a0b0538ee5c1a2be20cf7f5fb5ff975cb489.

--- src/texture.c
+++ src/texture.c
@@ -12,8 +12,8 @@ GLuint loadTexture(char *texturePath, int wrapS, int wrapT)
 	glBindTexture(GL_TEXTURE_2D, texture);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapS);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	int width, height, nrChannels;
 
