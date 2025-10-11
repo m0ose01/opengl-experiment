3e82a6d8b084f3567117da41735a0d6e73807219
Seamus <m0ose01@proton.me>
2025-10-11 19:15:27 +0100

    Implement Gourad shading.

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,38 +1,9 @@
 #version 330 core
 
-in vec3 Normal;
-in vec3 FragPos;
-out vec4 FragColor;
-
-uniform vec3 objectColour;
-uniform vec3 lightColour;
-uniform vec3 lightPos;
-// uniform vec3 viewPos;
-
-uniform float ambientStrength;
-uniform float diffuseStrength;
-uniform float specularStrength;
-uniform float shininess;
-
-uniform mat4 view;
+in vec3 FragColor;
+out vec4 FragColor2;
 
 void main()
 {
-
-	vec3 norm = normalize(Normal);
-	vec3 lightDir = normalize(vec3(view * vec4(lightPos, 1.0)) - FragPos);
-
-	float diff = max(dot(norm, lightDir), 0.0);
-	vec3 diffuse = diff * lightColour * diffuseStrength;
-
-	vec3 ambient = ambientStrength * lightColour;
-
-	vec3 viewDir = normalize(-FragPos);
-	vec3 reflectDir = reflect(-lightDir, norm);
-
-	float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
-	vec3 specular = specularStrength * spec * lightColour;
-
-	vec3 result = (ambient + diffuse + specular) * objectColour;
-	FragColor = vec4(result, 1.0);
+	FragColor2 = vec4(FragColor, 1.0);
 }
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -7,13 +7,36 @@ uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-out vec3 FragPos;
-out vec3 Normal;
+uniform vec3 objectColour;
+uniform vec3 lightColour;
+uniform vec3 lightPos;
+
+uniform float ambientStrength;
+uniform float diffuseStrength;
+uniform float specularStrength;
+uniform float shininess;
+
+out vec3 FragColor;
 
 void main()
 {
 	gl_Position = projection * view * model * vec4(aPos, 1.0);
-	FragPos = vec3(view * model * vec4(aPos, 1.0));
-	Normal = mat3(transpose(inverse(view * model))) * aNormal;
+	vec3 Normal = mat3(transpose(inverse(view * model))) * aNormal;
+	vec3 Position = vec3(view * model * vec4(aPos, 1.0));
+
+	vec3 ambient = ambientStrength * lightColour;
+
+	vec3 norm = normalize(Normal);
+	vec3 lightDir = normalize(vec3(view * vec4(lightPos, 1.0)) - Position);
+	float diff = max(dot(norm, lightDir), 0.0);
+	vec3 diffuse = diff * lightColour * diffuseStrength;
+
+	vec3 viewDir = normalize(-Position);
+	vec3 reflectDir = reflect(-lightDir, norm);
+
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
+	vec3 specular = specularStrength * spec * lightColour;
+
+	FragColor = (ambient + diffuse + specular) * objectColour;
 }
 
