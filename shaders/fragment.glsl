8d088638ffb7890c3f38b18577637ba8b222c5cd
Seamus <m0ose01@proton.me>
2025-10-11 18:32:56 +0100

    Do lighting calculations in view space.

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -7,25 +7,27 @@ out vec4 FragColor;
 uniform vec3 objectColour;
 uniform vec3 lightColour;
 uniform vec3 lightPos;
-uniform vec3 viewPos;
+// uniform vec3 viewPos;
 
 uniform float ambientStrength;
 uniform float diffuseStrength;
 uniform float specularStrength;
 uniform float shininess;
 
+uniform mat4 view;
+
 void main()
 {
 
 	vec3 norm = normalize(Normal);
-	vec3 lightDir = normalize(lightPos - FragPos);
+	vec3 lightDir = normalize(vec3(view * vec4(lightPos, 1.0)) - FragPos);
 
 	float diff = max(dot(norm, lightDir), 0.0);
 	vec3 diffuse = diff * lightColour * diffuseStrength;
 
 	vec3 ambient = ambientStrength * lightColour;
 
-	vec3 viewDir = normalize(viewPos - FragPos);
+	vec3 viewDir = normalize(-FragPos);
 	vec3 reflectDir = reflect(-lightDir, norm);
 
 	float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -13,7 +13,7 @@ out vec3 Normal;
 void main()
 {
 	gl_Position = projection * view * model * vec4(aPos, 1.0);
-	FragPos = vec3(model * vec4(aPos, 1.0));
-	Normal = mat3(transpose(inverse(model))) * aNormal;
+	FragPos = vec3(view * model * vec4(aPos, 1.0));
+	Normal = mat3(transpose(inverse(view * model))) * aNormal;
 }
 
