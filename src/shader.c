2e77718d119ae492e2880d1aeb0e26daca5ff5cb
Séamus O'Sullivan <seamus.osullivan@crick.ac.uk>
2025-11-04 23:17:51 +0000

    Rewrite shader code.
    Rewrite shader import code with improved error handling.

--- src/shader.c
+++ src/shader.c
@@ -1,88 +1,68 @@
 #include <glad/glad.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-GLuint loadShader(const char *vertexPath, const char *fragmentPath)
+GLuint loadShader(const char *vertex_path, const char *fragment_path)
 {
-	FILE *vertexFile = fopen(vertexPath, "rb");
-	if (vertexFile == NULL)
+	const char *shader_paths[] = {vertex_path, fragment_path};
+	const GLuint shader_types[] = {GL_VERTEX_SHADER, GL_FRAGMENT_SHADER};
+	GLuint shaders[2] = { 0 };
+
+	GLuint shader_program = glCreateProgram();
+
+	for (int current_shader = 0; current_shader < 2; current_shader++)
 	{
-		fprintf(stderr, "Error loading vertex shader file: %s.\n", vertexPath);
-		return 0;
+		FILE *current_shader_file = fopen(shader_paths[current_shader], "rb");
+		if (current_shader_file == NULL)
+		{
+			fprintf(stderr, "Error opening shader file: '%s'\n", shader_paths[current_shader]);
+			return 0;
+		}
+
+		// Get shader source file size
+		fseek(current_shader_file, 0, SEEK_END);
+		long file_size = ftell(current_shader_file);
+		fseek(current_shader_file, 0, SEEK_SET);
+
+		// using calloc and allocating an extra byte will automatically null-terminate the string.
+		char *shader_source = calloc(1, file_size + 1);
+		if (fread(shader_source, file_size, 1, current_shader_file) != 1)
+		{
+			fprintf(stderr, "Error reading shader file '%s'\n", shader_paths[current_shader]);
+			return 0;
+		}
+		fclose(current_shader_file);
+
+		shaders[current_shader] = glCreateShader(shader_types[current_shader]);
+		glShaderSource(shaders[current_shader], 1, &shader_source, NULL);
+		glCompileShader(shaders[current_shader]);
+		free(shader_source);
+
+		int compilation_successful = 0;
+		char log[512] = { 0 };
+		glGetShaderiv(shaders[current_shader], GL_COMPILE_STATUS, &compilation_successful);
+		if (!compilation_successful)
+		{
+			glGetShaderInfoLog(shaders[current_shader], 512, NULL, log);
+			fprintf(stderr, "Error compiling shader file '%s'. See log for more info:\n '%s'\n", shader_paths[current_shader], log);
+			return 0;
+		}
+
+		glAttachShader(shader_program, shaders[current_shader]);
 	}
-	fseek(vertexFile, 0, SEEK_END);
-	long fileSize = ftell(vertexFile);
-	fseek(vertexFile, 0, SEEK_SET);
 
-	char *vertexShaderSource = calloc(1, fileSize + 1);
-	if (fread(vertexShaderSource, fileSize, 1, vertexFile) != 1)
+	glLinkProgram(shader_program);
+
+	int linking_successful = 0;
+	char log[512] = { 0 };
+	glGetProgramiv(shader_program, GL_LINK_STATUS, &linking_successful);
+	if (!linking_successful)
 	{
-		fprintf(stderr, "Error reading vertex shader file.\n");
-		return 0;
-	}
-
-
-	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
-
-	int success;
-	char log[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, log);
-		printf("Error compiling vertex shader: %s\n", log);
-	}
-
-	FILE *fragmentFile = fopen(fragmentPath, "rb");
-	if (fragmentFile == NULL)
-	{
-		fprintf(stderr, "Error loading fragment shader file: %s.\n", fragmentPath);
-		return 0;
-	}
-	fseek(fragmentFile, 0, SEEK_END);
-	fileSize = ftell(fragmentFile);
-	fseek(fragmentFile, 0, SEEK_SET);
-
-	char *fragmentShaderSource = calloc(1, fileSize + 1);
-	if (fread(fragmentShaderSource, fileSize, 1, fragmentFile) != 1)
-	{
-		fprintf(stderr, "Error reading fragment shader file.\n");
-		return 0;
-	}
-
-	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-
-	memset(log, 0, sizeof(log));
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(fragmentShader, 512, NULL, log);
-		fprintf(stderr, "Error compiling fragment shader: %s\n", log);
+		glGetProgramInfoLog(shader_program, 512, NULL, log);
+		fprintf(stderr, "Error linking shader. See log for more info:\n, '%s'\n", log);
 		return 0;
 	}
 
-	GLuint programID = glCreateProgram();
-	glAttachShader(programID, vertexShader);
-	glAttachShader(programID, fragmentShader);
-	glLinkProgram(programID);
-
-	glLinkProgram(programID);
-
-	memset(log, 0, sizeof(log));
-	glGetProgramiv(programID, GL_LINK_STATUS, &success);
-	if (!success)
-	{
-		glGetProgramInfoLog(programID, 512, NULL, log);
-		fprintf(stderr, "Error linking shader program: %s\n", log);
-		return 0;
-	}
-
-	return programID;
+	return shader_program;
 }
 
